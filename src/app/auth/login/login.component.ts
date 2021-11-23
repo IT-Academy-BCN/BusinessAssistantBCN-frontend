8380e2238b01d662cf9963812fe1e872bd86c5bc
Roman Kostyuchenko <elromankos@gmail.com>
2021-11-23 12:17:08 +0100

    Interface Login Form and User Model
    sweetalert2 npm update

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
---
+++ src/app/models/usuario.model.ts
@@ -0,0 +1,14 @@
+
+export class Usuario {
+
+    constructor(
+        public nombre: string,
+        public email: string,
+        public password?: string,
+        public img?: string,
+        public role?: string,
+        public uid?: string,
+    ) {}
+
+
+}
