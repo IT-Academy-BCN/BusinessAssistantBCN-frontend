560bf95954c4e342a56357330cfeb39efbf8f7b8
Roman Kostyuchenko <elromankos@gmail.com>
2021-11-24 12:30:37 +0100

    Headers and reqClone create

--- src/app/helpers/interceptor.service.ts
+++ src/app/helpers/interceptor.service.ts
@@ -1,4 +1,4 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -10,8 +10,16 @@ export class InterceptorService implements HttpInterceptor{
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log('Passed through interceptor');
-    return next.handle( req );
+    
+    const headers = new HttpHeaders({
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDM0YjE4MC1mOTI0LTExZWItYTdlMS0wNmMwZjczZTU1ODQiLCJpYXQiOjE2MzcwNTcxMTksImV4cCI6MTYzNzA2MDcxOX0.gKOc8neofhh4uGy5_NQs5qPzYOaNkYJrky7BbkAipi0'
+    });
+
+    const reqClone = req.clone({
+      headers
+    });
+
+    return next.handle( reqClone );
     
   }
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -32,6 +32,8 @@ export class UsuarioService {
 
   }
 
+  //obtenerUsuarios() {}
+
   
   logout() {
 
