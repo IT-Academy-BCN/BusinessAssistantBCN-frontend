66985aace1c38c0bfab285c9c178f249b5789acf
Roman Kostyuchenko <elromankos@gmail.com>
2021-11-30 11:39:57 +0100

    View component of Login page works
    import of dependencies and libraries
    html login create
    working with <form>

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
@@ -10,7 +10,7 @@ import Swal from 'sweetalert2';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   public formSubmitted = false;
 
