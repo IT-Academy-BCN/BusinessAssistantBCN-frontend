95103db2dc5014cab6fd2aeb01165e67f29f2c88
jordi <jordicalatayudcastro@gmail.com>
2021-12-01 10:32:22 +0100

    create componnets and markup

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+
+
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class SharedModule { }
