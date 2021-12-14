695864ae5af1472484d408c1541fb29297f2b63d
Roman Kostyuchenko <elromankos@gmail.com>
2021-12-14 23:18:24 +0100

    routes path create
    /login
    /menu
    /big-stablishments-detail
    /big-stablishments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,14 +6,23 @@ import { JwtInterceptor } from "./helpers/JwtInterceptor";
 
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
 import { MyEnvironmentPageComponent } from './modules/home/my-environment-page/my-environment-page.component';
+import { LoginFormComponent } from './modules/login/login-form/login-form.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { BigStablishmentsDetailPageComponent } from './modules/home/big-stablishments-detail-page/big-stablishments-detail-page.component';
+import { BigStablishmentsPageComponent } from './modules/home/big-stablishments-page/big-stablishments-page.component';
 
 
 const routes: Routes = [
   { path: "", redirectTo: 'home', pathMatch: 'full' },
   { path: "home", component: HomePageComponent },
   { path: "directory", component: MyEnvironmentPageComponent },
-  { path: "home", component: HomePageComponent },
-  { path: "directory", component: MyEnvironmentPageComponent },
+  { path: "login", component: LoginFormComponent },
+  { path: "menu", component: MenuComponent },
+  { path: "big-stablishments-detail", component: BigStablishmentsDetailPageComponent },
+  { path: "big-stablishments", component: BigStablishmentsPageComponent },
+  
+  
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
 import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './modules/home/home.module';
 import { LoginModule } from './modules/login/login.module';
+
+import { AppComponent } from './app.component';
 
 
 
@@ -25,7 +29,7 @@ import { LoginModule } from './modules/login/login.module';
     HomeModule,
     LoginModule
   ],
-  providers: [],
+ 
   bootstrap: [AppComponent]
 })
 export class AppModule {
