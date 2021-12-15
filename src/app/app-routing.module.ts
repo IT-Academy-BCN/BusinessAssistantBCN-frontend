6dc321870ae37f951b6a0844ca381651900e74e9
jordi <jordicalatayudcastro@gmail.com>
2021-12-15 13:49:14 +0100

    Login markup using @ng-bootsrap/ needs logic still

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { JwtInterceptor } from "./helpers/JwtInterceptor";
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
 import { MyEnvironmentPageComponent } from './modules/home/my-environment-page/my-environment-page.component';
+import { LoginFormComponent } from './modules/login/login-form/login-form.component';
 
 
 const routes: Routes = [
   { path: "", redirectTo: 'home', pathMatch: 'full' },
   { path: "home", component: HomePageComponent },
   { path: "myEnvironment", component: MyEnvironmentPageComponent }
+
 ];
 
 @NgModule({
