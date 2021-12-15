e1f2b73c46a502d4b77f353cde52348fb92767d4
Roman Kostyuchenko <elromankos@gmail.com>
2021-12-15 23:38:01 +0100

    Virtual Assistant navigation update
    Virtual assistant works
    Shared components selectors settled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { JwtInterceptor } from "./helpers/JwtInterceptor";
 import { HomePageComponent } from './modules/home/home-page/home-page.component';
 import { MyEnvironmentPageComponent } from './modules/home/my-environment-page/my-environment-page.component';
 import { LoginFormComponent } from './modules/login/login-form/login-form.component';
+import { BigStablishmentsDetailPageComponent } from './modules/home/big-stablishments-detail-page/big-stablishments-detail-page.component';
+import { BigStablishmentsPageComponent } from './modules/home/big-stablishments-page/big-stablishments-page.component';
+import { VirtualAsisstantComponent } from './modules/home/home-components/virtual-asisstant/virtual-asisstant.component';
 
 
 const routes: Routes = [
   { path: "", redirectTo: 'home', pathMatch: 'full' },
   { path: "home", component: HomePageComponent },
   { path: "login", component: LoginFormComponent },
-  { path: "myEnvironment", component: MyEnvironmentPageComponent }
+  { path: "myEnvironment", component: MyEnvironmentPageComponent },
+  { path: "virtual-assistant", component: VirtualAsisstantComponent },
+  { path: "bis-stablishments", component: BigStablishmentsPageComponent },
+  { path: "big-stablishments-detail", component: BigStablishmentsDetailPageComponent }
 ];
 
 @NgModule({
