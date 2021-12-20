112bb1820989731e7cafcbd6a229d7d749dfedb3
jordi <jordicalatayudcastro@gmail.com>
2021-12-20 19:06:32 +0100

    Reactive form logic &  Authtentication service created

--- src/app/modules/login/login-form/login-form.component.ts
+++ src/app/modules/login/login-form/login-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login-form',
@@ -10,11 +12,17 @@ import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
 export class LoginFormComponent implements OnInit {
 
 
-  constructor(public activeModal: NgbActiveModal, private router: Router) { }
+  constructor(public activeModal: NgbActiveModal, private router: Router,
+    private f: FormBuilder) {
+  }
+
+  loginForm!: FormGroup;
+  submitted = false;
 
 
   ngOnInit(): void {
     this.changingLoginQueryParams();
+    this.createLoginForm();
   }
 
   //Change params URL Login component Modal
@@ -33,5 +41,32 @@ export class LoginFormComponent implements OnInit {
   public closeLoginForm() {
     this.activeModal.close(LoginFormComponent);
     this.router.navigate(['/home']);
+  }
+
+  public createLoginForm() {
+    this.loginForm = this.f.group({
+      email: [" ", [Validators.required, Validators.email]],
+      password: [" ", [Validators.required]]
+    }, { updateOn: 'submit' });
+  }
+
+  // Getter method to access formcontrols
+  get getControl(): { [key: string]: AbstractControl; } {
+    return this.loginForm.controls;
+  }
+
+  //Submit Login form
+  public submitForm() {
+    this.submitted = true;
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.resetForm();
+      return this.loginForm.value;
+    }
+  }
+  //Reset Login Form
+  public resetForm() {
+    this.loginForm.reset();
+    this.submitted = false;
   }
 }
