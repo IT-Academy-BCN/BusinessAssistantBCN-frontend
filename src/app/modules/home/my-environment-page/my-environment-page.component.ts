ba52234aad3ec43a8546cdf9449923352e05ef31
Roman Kostyuchenko <elromankos@gmail.com>
2021-12-22 12:57:19 +0100

    Navigation continue
    Correct path large establishments
    Button add on icon, routerLink to navigate works
    Property correction in meEnviroment component

--- src/app/modules/home/my-environment-page/my-environment-page.component.ts
+++ src/app/modules/home/my-environment-page/my-environment-page.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class MyEnvironmentPageComponent implements OnInit {
 
 
-  bigStablihsments = "assets/img/shopping-mall.png";
+  largeStablihsments = "assets/img/shopping-mall.png";
   shoppingMall = "assets/img/market.png";
   shoppingCart = "assets/img/mall-shopping-cart.png";
   comercialMarket = "assets/img/shop.png";
