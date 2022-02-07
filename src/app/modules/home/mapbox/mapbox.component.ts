8939072b0426e32e661e43ced533aaaf658c0400
Roman Kostyuchenko <elromankos@gmail.com>
2022-02-08 00:48:09 +0100

    Map works
    MapBox gl update
    dependecies install
    tsconfig add allowSyntheticDefaultImports
    AfterViewInit implement
    Local Ref add
    Map Styling

--- src/app/modules/home/mapbox/mapbox.component.ts
+++ src/app/modules/home/mapbox/mapbox.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'app-mapbox',
   templateUrl: './mapbox.component.html',
   styleUrls: ['./mapbox.component.css']
 })
-export class MapboxComponent implements OnInit {
+export class MapboxComponent implements AfterViewInit {
+
+  @ViewChild('mapDiv')
+  mapDivElement!: ElementRef
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+
+    const map = new Map({
+      container: this.mapDivElement.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: [ 2.19516507807684, 41.40250915633661 ], // starting position [lng, lat]
+      zoom: 14 // starting zoom
+      });
+      
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiZWxyb21hbmtvcyIsImEiOiJja3pkYjMzdnowNmVjMnVvY3NqcXdkYjVzIn0.NPq1kPmoJAnGTQmRmPDn2A';
 
 if (environment.production) {
   enableProdMode();
