254fcf17b8053f10f60ab276d6cdc513a09bc85b
Voluntata <voluntata@gmail.com>
2022-02-28 17:02:26 +0100

    correction BABCN-17

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   BACKEND_LOGIN_URL: '/login',
   BACKEND_ZONES_URL: '../../assets/dummy/zones_dummy.json',
   BACKEND_LARGE_STABLISHMENTS_ACTIVITIES_URL: '../../assets/dummy/activities_dummy.json',
-  BACKEND_LARGE_STABLISHMENTS_ACTIVITY_URL: '../../assets/dummy/big-malls_dummy.json',
+  BACKEND_LARGE_STABLISHMENTS_ACTIVITY_URL: '../../assets/dummy/large-establishments_dummy.json',
   BACKEND_LARGE_STABLISHMENTS_DISTRICT_URL: '',
   MAPBOX_TOKEN: 'pk.eyJ1IjoianZyZnJlZWxhbmNlZGV2ZWxvcGVyIiwiYSI6ImNreTl4czUzMTAwNGQydnFsdmRhYXRvbDUifQ.TVL-2T184QdfXbze6VNw4A'
 };
