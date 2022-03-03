24467ac08ed1281df92fcb5fe040ff264de60ab0
Voluntata <voluntata@gmail.com>
2022-03-03 16:29:46 +0100

    BABCN-25, correction

--- src/app/modules/home/large-stablishments-page/large-stablishments-page.component.ts
+++ src/app/modules/home/large-stablishments-page/large-stablishments-page.component.ts
@@ -1,12 +1,126 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from "rxjs";
-import { ActivatedRoute, Router } from "@angular/router";
+// import { Component, OnDestroy, OnInit } from '@angular/core';
+// import { Subscription } from "rxjs";
+// import { ActivatedRoute, Router } from "@angular/router";
+// import { LargeStablishmentsService } from '../../../services/large-stablishments.service';
+// import { CommonService } from "../../../services/common.service";
+// import { ZoneModel } from "../../../models/common/zone.model";
+// import { EconomicActivityModel } from 'src/app/models/common/economic-activity.model';
+// import { LargeStablishmentModel } from '../../../models/large-stablishment.model';
+// import { FilteredDataModel } from 'src/app/models/common/filtered-data.model';
+
+// @Component({
+//   selector: 'app-large-stablishments-page',
+//   templateUrl: './large-stablishments-page.component.html',
+//   styleUrls: ['./large-stablishments-page.component.css']
+// })
+// export class LargeStablishmentsPageComponent implements OnInit, OnDestroy {
+
+
+//   //Subscriptions
+//   zones$!: Subscription;
+//   activities$!: Subscription;
+
+//   //Other elements
+//   bcnZones: ZoneModel[] = [];
+//   largeStablishmentActivities: EconomicActivityModel[] = [];
+
+//   //Options checked
+//   largeStablishmentActivitiesSelected: EconomicActivityModel[] = [];
+//   idZonaSelected: number[] = [];
+//   idActivitySelected: number[] = [];
+//   largeStablishmentselected: FilteredDataModel = new FilteredDataModel(this.idZonaSelected, this.idActivitySelected)
+//   largeStablishmentSelectedJSON = '';
+
+//   get bcnZonesSelected() {
+//     return this.largeStablishmentsService.bcnZonesSelected;
+//   }
+
+//   constructor(private router: Router,
+//     private activatedRoute: ActivatedRoute,
+//     private largeStablishmentsService: LargeStablishmentsService,
+//     private commonservice: CommonService) { }
+
+//   ngOnInit(): void {
+//     this.loadMasterData();
+//     this.largeStablishmentsService.initializeZonesSelected();
+//   }
+
+//   ngOnDestroy() {
+//     if (this.zones$ != undefined) this.zones$.unsubscribe();
+//   }
+
+//   loadMasterData() {
+//     this.zones$ = this.commonservice.getZones().subscribe(resp => {
+
+//       // console.log(resp);
+//       resp.results.forEach((element: any) => {
+//         const bcnZone: ZoneModel = new ZoneModel(element);
+//         this.bcnZones.push(bcnZone);
+//       });
+//     });
+//     this.activities$ = this.commonservice.getEconomicActivities().subscribe(resp => {
+
+//       // console.log(resp);
+//       resp.results.forEach((element: any) => {
+//         const largeStablishmentActivity: EconomicActivityModel = new EconomicActivityModel(element);
+//         this.largeStablishmentActivities.push(largeStablishmentActivity);
+//       });
+//     });
+//   }
+
+//   largeStablishmentZonesSelected(zoneSelected: ZoneModel, event: any) {
+//     if (event.checked) {
+//       this.largeStablishmentsService.addZonesSelected(zoneSelected);
+//     } else {
+//       this.largeStablishmentsService.deleteZoneSelected(zoneSelected);
+//     }
+//     console.log(this.bcnZonesSelected);
+//     this.idZonaSelected = this.bcnZonesSelected.map(data => data.idZone);
+//     console.log(this.idZonaSelected);
+//   }
+
+//   largeStablishmentActivitySelected(activitySelected: EconomicActivityModel, event: any) {
+//     if (event.checked) {
+//       this.largeStablishmentActivitiesSelected.push(activitySelected);
+//     } else {
+//       this.deleteActivitySelected(activitySelected);
+//     }
+//     // console.log(this.largeStablishmentActivitiesSelected);
+//     this.idActivitySelected = this.largeStablishmentActivitiesSelected.map(data => data.idActivity);
+//     console.log(this.idActivitySelected)
+//   }
+
+//   deleteActivitySelected(activitySelected: EconomicActivityModel) {
+//     this.largeStablishmentActivitiesSelected.map((activity, index) => {
+//       if (activity === activitySelected) {
+//         this.largeStablishmentActivitiesSelected.splice(index, 1);
+//       }
+//     });
+//   }
+
+
+//   largeStablishmentSearch() {
+  
+//     this.largeStablishmentselected.activities = this.idActivitySelected;
+//     this.largeStablishmentselected.zones = this.idZonaSelected;
+//     console.log(this.largeStablishmentselected)
+//     this.largeStablishmentSelectedJSON = JSON.stringify(this.largeStablishmentselected);
+//    console.log( this.largeStablishmentsService.sendSelectedData())
+//    //console.log(this.largeStablishmentSelectedJSON)
+//   }
+
+//   largeStablishmentActivitySearch() {
+//     console.log(this.largeStablishmentActivities);
+//   }
+// }
+
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Router} from "@angular/router";
 import { LargeStablishmentsService } from '../../../services/large-stablishments.service';
-import { CommonService } from "../../../services/common.service";
-import { ZoneModel } from "../../../models/common/zone.model";
+import { CommonService} from "../../../services/common.service";
+import {ZoneModel} from "../../../models/common/zone.model";
 import { EconomicActivityModel } from 'src/app/models/common/economic-activity.model';
-import { LargeStablishmentModel } from '../../../models/large-stablishment.model';
-import { FilteredDataModel } from 'src/app/models/common/filtered-data.model';
 
 @Component({
   selector: 'app-large-stablishments-page',
@@ -21,24 +135,24 @@ export class LargeStablishmentsPageComponent implements OnInit, OnDestroy {
   activities$!: Subscription;
 
   //Other elements
-  bcnZones: ZoneModel[] = [];
-  largeStablishmentActivities: EconomicActivityModel[] = [];
+  bcnZones: ZoneModel [] = [];
+  largeStablishmentActivities: EconomicActivityModel [] = [];
 
   //Options checked
-  largeStablishmentActivitiesSelected: EconomicActivityModel[] = [];
-  idZonaSelected: number[] = [];
-  idActivitySelected: number[] = [];
-  largeStablishmentselected: FilteredDataModel = new FilteredDataModel(this.idZonaSelected, this.idActivitySelected)
-  largeStablishmentSelectedJSON = '';
+  largeStablishmentActivitiesSelected: EconomicActivityModel [] = [];
 
   get bcnZonesSelected() {
     return this.largeStablishmentsService.bcnZonesSelected;
   }
 
+  get activitiiesSelected(){
+    return this.largeStablishmentsService.activitiesSelected;
+  }
+
   constructor(private router: Router,
-    private activatedRoute: ActivatedRoute,
-    private largeStablishmentsService: LargeStablishmentsService,
-    private commonservice: CommonService) { }
+              private activatedRoute: ActivatedRoute,
+              private largeStablishmentsService: LargeStablishmentsService,
+              private commonservice: CommonService) { }
 
   ngOnInit(): void {
     this.loadMasterData();
@@ -46,72 +160,55 @@ export class LargeStablishmentsPageComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    if (this.zones$ != undefined) this.zones$.unsubscribe();
+    if( this.zones$ != undefined ) this.zones$.unsubscribe();
   }
 
-  loadMasterData() {
-    this.zones$ = this.commonservice.getZones().subscribe(resp => {
+  loadMasterData(){
+    this.zones$ = this.commonservice.getZones().subscribe( resp => {
 
       // console.log(resp);
-      resp.results.forEach((element: any) => {
-        const bcnZone: ZoneModel = new ZoneModel(element);
+      resp.results.forEach ( (element:any) => {
+        const bcnZone:ZoneModel = new ZoneModel(element);
         this.bcnZones.push(bcnZone);
       });
     });
-    this.activities$ = this.commonservice.getEconomicActivities().subscribe(resp => {
+    this.activities$ = this.commonservice.getEconomicActivities().subscribe( resp => {
 
       // console.log(resp);
-      resp.results.forEach((element: any) => {
-        const largeStablishmentActivity: EconomicActivityModel = new EconomicActivityModel(element);
+      resp.results.forEach ( (element:any) => {
+        const largeStablishmentActivity:EconomicActivityModel = new EconomicActivityModel(element);
         this.largeStablishmentActivities.push(largeStablishmentActivity);
       });
     });
   }
 
   largeStablishmentZonesSelected(zoneSelected: ZoneModel, event: any) {
-    if (event.checked) {
+    if(event.checked){
       this.largeStablishmentsService.addZonesSelected(zoneSelected);
-    } else {
+    }else{
       this.largeStablishmentsService.deleteZoneSelected(zoneSelected);
     }
     console.log(this.bcnZonesSelected);
-    this.idZonaSelected = this.bcnZonesSelected.map(data => data.idZone);
-    console.log(this.idZonaSelected);
   }
 
   largeStablishmentActivitySelected(activitySelected: EconomicActivityModel, event: any) {
-    if (event.checked) {
-      this.largeStablishmentActivitiesSelected.push(activitySelected);
-    } else {
-      this.deleteActivitySelected(activitySelected);
+    if(event.checked){
+      this.largeStablishmentsService.addActivitiesSelected(activitySelected)
+    }else{
+      this.largeStablishmentsService.deleteActivitySelected(activitySelected)
     }
-    // console.log(this.largeStablishmentActivitiesSelected);
-    this.idActivitySelected = this.largeStablishmentActivitiesSelected.map(data => data.idActivity);
-    console.log(this.idActivitySelected)
+    console.log(this.activitiiesSelected);
   }
 
-  deleteActivitySelected(activitySelected: EconomicActivityModel) {
-    this.largeStablishmentActivitiesSelected.map((activity, index) => {
-      if (activity === activitySelected) {
-        this.largeStablishmentActivitiesSelected.splice(index, 1);
-      }
-    });
-  }
 
 
   largeStablishmentSearch() {
-    //  console.log( this.bcnZones );
-    this.largeStablishmentselected.activities = this.idActivitySelected;
-    this.largeStablishmentselected.zones = this.idZonaSelected;
-    // console.log(this.largeStablishmentselected)
-    this.largeStablishmentSelectedJSON = JSON.stringify(this.largeStablishmentselected);
-    this.largeStablishmentsService.sendSelectedData(this.largeStablishmentSelectedJSON);
-   // console.log(this.largeStablishmentSelectedJSON)
-
+   // console.log( this.bcnZones );
+ this.largeStablishmentsService.sendSelectedData();
 
   }
 
   largeStablishmentActivitySearch() {
-    console.log(this.largeStablishmentActivities);
+    console.log( this.largeStablishmentActivities );
   }
 }
