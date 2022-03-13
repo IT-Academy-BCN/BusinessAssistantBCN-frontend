667d97b213b5ebf2cb51a8539c5f1a0de19fe109
Voluntata <voluntata@gmail.com>
2022-03-13 13:37:44 +0100

    saved-search component

---
+++ src/app/modules/home/saved-searches/saved-searches.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-searches',
+  templateUrl: './saved-searches.component.html',
+  styleUrls: ['./saved-searches.component.css']
+})
+export class SavedSearchesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
