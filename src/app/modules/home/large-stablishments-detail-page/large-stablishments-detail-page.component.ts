bf8588b1fa83768de6750508c1c26e4105151923
Jordi Riera <jordiriera81@gmail.com>
2022-03-15 11:54:04 +0100

    Feature:Add pdf generation with pdfmake library

--- src/app/modules/home/large-stablishments-detail-page/large-stablishments-detail-page.component.ts
+++ src/app/modules/home/large-stablishments-detail-page/large-stablishments-detail-page.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core'
 import { LargeStablishmentsService } from 'src/app/services/large-stablishments.service'
 import { LargeStablishmentModel } from '../../../models/large-stablishment.model';
-import jsPDF from "jspdf"
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+
 
 
 @Component({
@@ -10,8 +14,10 @@ import jsPDF from "jspdf"
   styleUrls: ['./large-stablishments-detail-page.component.css'],
 })
 export class LargeStablishmentsDetailPageComponent implements OnInit {
-  LSData: LargeStablishmentModel[] = []
-  file = new jsPDF();
+  LSData: LargeStablishmentModel[] = [];
+
+
+
 
 
   constructor(private lSservice: LargeStablishmentsService) {}
@@ -23,30 +29,15 @@ export class LargeStablishmentsDetailPageComponent implements OnInit {
     })
   }
 
-  openPDF(): void {
-      /*Simplify object array to match data on view*/
-      const clone = this.LSData.map(ls=>{
+  generateDocument(){
+    const clone = this.LSData.map(ls=>{
       const simpleLS:any = Object.assign({},ls);
       delete simpleLS.activities;
       return simpleLS;
     })
-      /*pdf library to define line wrapping*/
-      let lines:string[] =this.file.splitTextToSize(JSON.stringify(clone,null,"\t"), 200);
-
-      /*Clean extra top lines*/
-      lines.splice(0,2);
-      let i=0
-      let j=this.LSData.length/2; //to define number of pages
-
-
-      while(i<j){
-
-        const next = lines.splice(0,38);// fits two objects per page
-        this.file.text(next,20,20);
-        this.file.addPage();
-        i++
-      }
-
-      this.file.save('BABCN_Search.pdf');
+    const docDefinition = {content:JSON.stringify(clone,null,"\t")}
+    pdfMake.createPdf(docDefinition).open();
   }
+
+
 }
