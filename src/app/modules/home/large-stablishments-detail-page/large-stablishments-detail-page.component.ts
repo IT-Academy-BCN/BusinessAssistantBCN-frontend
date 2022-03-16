aea031316ceeb38e3c6975a9a1d39a5d5dc5c81a
Jordi Riera <jordiriera81@gmail.com>
2022-03-16 02:32:40 +0100

    Revert "Fix:remove jsPDF dependency"
    This reverts commit 9dfa1e6f0c5b35ebf7a6c9f755b2e813eda9750f.

--- src/app/modules/home/large-stablishments-detail-page/large-stablishments-detail-page.component.ts
+++ src/app/modules/home/large-stablishments-detail-page/large-stablishments-detail-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { LargeStablishmentsService } from 'src/app/services/large-stablishments.service'
 import { LargeStablishmentModel } from '../../../models/large-stablishment.model';
-
+import jsPDF from "jspdf"
 
 
 @Component({
@@ -11,7 +11,7 @@ import { LargeStablishmentModel } from '../../../models/large-stablishment.model
 })
 export class LargeStablishmentsDetailPageComponent implements OnInit {
   LSData: LargeStablishmentModel[] = []
-
+  file = new jsPDF();
 
 
   constructor(private lSservice: LargeStablishmentsService) {}
@@ -23,5 +23,30 @@ export class LargeStablishmentsDetailPageComponent implements OnInit {
     })
   }
 
+  openPDF(): void {
+      /*Simplify object array to match data on view*/
+      const clone = this.LSData.map(ls=>{
+      const simpleLS:any = Object.assign({},ls);
+      delete simpleLS.activities;
+      return simpleLS;
+    })
+      /*pdf library to define line wrapping*/
+      let lines:string[] =this.file.splitTextToSize(JSON.stringify(clone,null,"\t"), 200);
 
+      /*Clean extra top lines*/
+      lines.splice(0,2);
+      let i=0
+      let j=this.LSData.length/2; //to define number of pages
+
+
+      while(i<j){
+
+        const next = lines.splice(0,38);// fits two objects per page
+        this.file.text(next,20,20);
+        this.file.addPage();
+        i++
+      }
+
+      this.file.save('BABCN_Search.pdf');
+  }
 }
