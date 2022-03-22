518fd7c0f1fab0c26e83ca52a967fc5f1582105a
ester <ester.perello.sentis@gmail.com>
2022-03-22 16:41:03 +0100

    better comments and removed duplicate line

--- src/app/shared/mapbox/mapbox.component.ts
+++ src/app/shared/mapbox/mapbox.component.ts
@@ -40,7 +40,7 @@ export class MapboxComponent implements AfterViewInit {
     this.map = new Map({
       container: this.mapDivElement.nativeElement,
       style: "mapbox://styles/mapbox/streets-v11", // style URL }
-      center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y],
+      center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], // starting center so it doesn't start from Germany
       zoom: 8
     });
 
@@ -52,18 +52,17 @@ export class MapboxComponent implements AfterViewInit {
         positionOptions: {
           enableHighAccuracy: true,
         },
-        // When active the map will receive updates to the device's location as it changes.
         trackUserLocation: true,
       })
     );
   }
 
   // Function to create a single marker (with the marker's colour and the business (or user's coords) as parameters)
-  createANewMarker(markerColor: string, element?: LargeStablishmentModel, coord?: GeolocationCoordinates): void {
+  createANewMarker(markerColor: string, business?: LargeStablishmentModel, coord?: GeolocationCoordinates): void {
 
-    // Create a popup with the businesse's basic information
+    // Create a popup with the business's basic information
     const popup = new Popup().setHTML(
-      `<b>${element?.name}</b> </br> ${element?.addresses[0].street_name} , ${element?.addresses[0].number}`
+      `<b>${business?.name}</b> </br> ${business?.addresses[0].street_name} , ${business?.addresses[0].number}`
     );
 
     if (coord) { // If user has accepted to share their location
@@ -71,41 +70,39 @@ export class MapboxComponent implements AfterViewInit {
         .setLngLat([coord.longitude, coord.latitude])
         .addTo(this.map);
       this.currentMarkers.push(newIndividualMarker);
-    } else { // If user hasn't accepted to share their location
+    } else { // If user hasn't accepted to share their location OR when iterating through the filteredResultsToPrintOnMap array.
       const newIndividualMarker = new Marker({ color: markerColor })
-        .setLngLat([element!.addresses[0].location.x, element!.addresses[0].location.y])
+        .setLngLat([business!.addresses[0].location.x, business!.addresses[0].location.y])
         .setPopup(popup)
         .addTo(this.map);
       this.currentMarkers.push(newIndividualMarker);
-
-      this.currentMarkers.push(newIndividualMarker);
-
-      // MAP LÍMITS
-      // Initial point 0
-      const bounds = new LngLatBounds();
-
-      // Add all the markers to the map's bounds. 
-      this.currentMarkers.forEach(marker =>
-        bounds.extend(marker.getLngLat()));
-
-      // Adjust the zoom to see all the existing markers
-      this.map.fitBounds(bounds, {
-        padding: 50
-      })
     }
+
+    // MAP LÍMITS
+    // Initial point 0
+    const bounds = new LngLatBounds();
+
+    // Add all the markers to the map's bounds. 
+    this.currentMarkers.forEach(marker =>
+      bounds.extend(marker.getLngLat()));
+
+    // Adjust the zoom to see all the existing markers
+    this.map.fitBounds(bounds, {
+      padding: 75
+    })
   }
 
   getUsersLocation(): void {
     navigator.geolocation.getCurrentPosition(
       // Success callback function (if user has accepted to share their location)
       (pos) => {
-        this.map.flyTo({ center: [pos.coords.longitude, pos.coords.latitude], zoom: 11 })
+        // this.map.flyTo({ center: [pos.coords.longitude, pos.coords.latitude], zoom: 11 })
         this.createANewMarker("green", undefined, pos.coords);
       },
       // Error callback function (if user hasn't accepted to share their location)
       () => {
-        this.map.flyTo(
-          { center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], zoom: 11 })
+        // this.map.flyTo(
+        //   { center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], zoom: 11 })
         this.createANewMarker("red", environment.MAPBOX_ITAcademy_OBJECT,);
       }
     );
