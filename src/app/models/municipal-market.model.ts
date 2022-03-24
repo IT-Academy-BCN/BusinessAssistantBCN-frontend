53f1307438e9631665ec8c59ee3cadf865ff87b8
recasmir <mireia.recasens@gmail.com>
2022-03-24 09:42:14 +0100

    missing api call, translations

--- src/app/models/municipal-market.model.ts
+++ src/app/models/municipal-market.model.ts
@@ -1,3 +1,24 @@
-export interface MunicipalMarketModel {
-
-}
+   export interface MunicipalMarketModel {
+      name:      string;
+      web:       string[];
+      email:     string[];
+      phone:     string[];
+      location: {
+          x: number;
+          y: number
+      };
+      addresses: Address[];
+  }
+  
+  export interface Address {
+      street_name:   string;
+      street_number: number;
+      zip_code:      string;
+      town:          string;
+      district_id:   string;
+  }
+  
+  // export interface Location {
+  //     x: number;
+  //     y: number;
+  // }
