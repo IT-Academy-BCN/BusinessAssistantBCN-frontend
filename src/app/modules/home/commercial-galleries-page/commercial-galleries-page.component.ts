ca7159fa0f563a91bda97ccd4f35bf609969907d
jriera <jordiriera81@gmail.com>
2022-04-19 10:11:09 +0200

    feat:Add specific commercial galleries service and component

--- src/app/modules/home/commercial-galleries-page/commercial-galleries-page.component.ts
+++ src/app/modules/home/commercial-galleries-page/commercial-galleries-page.component.ts
@@ -1,15 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { EconomicActivityModel } from 'src/app/models/common/economic-activity.model';
+import { ZoneModel } from 'src/app/models/common/zone.model';
+import { CommonService } from 'src/app/services/common.service';
+import {CommercialGalleriesService} from "../../../services/commercial-galleries.service";
 
 @Component({
-  selector: 'app-commercial-galleries-page',
+  selector: 'commercial-galleries-page',
   templateUrl: './commercial-galleries-page.component.html',
   styleUrls: ['./commercial-galleries-page.component.css']
 })
-export class CommercialGalleriesPageComponent implements OnInit {
+export class CommercialGalleriesPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+      private commonService: CommonService,
+      private commercialGalleriesService: CommercialGalleriesService,
+      private fb:FormBuilder
+  ) { }
 
   ngOnInit(): void {
+
+    this.zones$ = this.commonService.getZones().subscribe( resp => {
+      this.zones = resp.results;
+      this.addFormArr('zonesForm', resp.results)
+    })
+
+    this.activities$ = this.commonService.getEconomicActivities().subscribe( resp => {
+      this.activities = resp.results;
+      this.addFormArr('activitiesForm', resp.results)
+    })
+
+  }
+
+  ngOnDestroy(): void {
+    this.zones$.unsubscribe();
+    this.activities$.unsubscribe();
+  }
+
+  zones: ZoneModel[] = [];
+  activities: EconomicActivityModel[]= [];
+
+  //Subscriptions
+  zones$!: Subscription;
+  activities$!: Subscription;
+
+  //Form
+  bigMallsForm: FormGroup = this.fb.group({
+    zonesForm: this.fb.array([]),
+    activitiesForm: this.fb.array([])
+  })
+
+  addFormArr( form: string, itemsArr: any[] ){
+    const formArr = this.bigMallsForm.get( form ) as FormArray;
+    itemsArr.forEach( (_) => {
+      formArr.push( this.fb.control(false) )
+    })
+  }
+
+  getFormArr( formArr: string ){
+    return this.bigMallsForm.get( formArr ) as FormArray;
+  }
+
+  // 
+  sendSelectedData() {
+    this.commercialGalleriesService.setZonesSelected( this.getZonesSelected() );
+    this.commercialGalleriesService.setActivitiesSelected( this.getActivitiesSelected() );
+
+    // Log selected results to check everything is ok! These logs should be deleted
+    console.log( 'Zones Selected: ', this.commercialGalleriesService.zoneSelected );
+    console.log( 'Activities Selected: ', this.commercialGalleriesService.activitiesSelected );
+  }
+
+  getZonesSelected(): ZoneModel[]{
+    return this.zones.filter( (zone, i) => {
+      if( this.getFormArr('zonesForm').controls[i].value ){
+        return zone
+      }
+      return
+    })
+  }
+
+  getActivitiesSelected(): EconomicActivityModel[]{
+    return this.activities.filter( (activity, i) => {
+      if( this.getFormArr('activitiesForm').controls[i].value ){
+        return activity
+      }
+      return
+    })
   }
 
 }
--- src/app/services/commercial-galleries.service.ts
+++ src/app/services/commercial-galleries.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
+import {HttpClient , HttpParams} from "@angular/common/http";
+import {ZoneModel} from "../models/common/zone.model";
+import {EconomicActivityModel} from "../models/common/economic-activity.model";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommercialGalleriesService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  private _zonesSelected: ZoneModel[] = [];
+  private _activitiesSelected: EconomicActivityModel[] = [];
+
+  get zoneSelected() {
+    return [...this._zonesSelected]
+  }
+
+  get activitiesSelected(){
+    return [...this._activitiesSelected]
+  }
+
+  setZonesSelected( zonesArr: ZoneModel[] ){
+    this._zonesSelected = zonesArr;
+  }
+
+  setActivitiesSelected( activitiesArr: EconomicActivityModel[] ){
+    this._activitiesSelected = activitiesArr
+  }
+
+  getSelectedData() {
+    let params = new HttpParams();
+    const zonesId = this._zonesSelected.map( zone => zone.idZone );
+    const activitiesId = this._activitiesSelected.map( activity => activity.idActivity )
+
+    params = params.append( 'zones', JSON.stringify(zonesId) )
+    params = params.append( 'activities', JSON.stringify(activitiesId) );
+
+    console.log(params)
+    // Fake-filtered to check that it works. Will have to be replaced for real backend response.
+    return this.http.get(`${environment.BACKEND_BASE_URL}${environment.BACKEND_COMMERCIAL_GALLERIES}`, { params: params },
+    )
+  }
 }
