424d610808658fc706789708e63503dd22c1ed83
jriera <jordiriera81@gmail.com>
2022-04-19 18:45:25 +0200

    refactor: remove unused imports

--- src/app/services/commercial-galleries.service.ts
+++ src/app/services/commercial-galleries.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient , HttpParams} from "@angular/common/http";
 import {ZoneModel} from "../models/common/zone.model";
-import {EconomicActivityModel} from "../models/common/economic-activity.model";
 import {environment} from "../../environments/environment";
-import {MunicipalMarketModel} from "../models/municipal-market.model";
 import {Router} from "@angular/router";
 import {CommercialGalleryModel} from "../models/commercial-gallery.model";
 
