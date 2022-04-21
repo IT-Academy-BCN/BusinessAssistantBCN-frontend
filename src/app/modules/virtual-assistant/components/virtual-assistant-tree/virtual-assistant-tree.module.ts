fd2896489b28839e1ba86326814b4e99c655b4a6
Ramon <rferrergrau@gmail.com>
2022-04-21 20:19:45 +0200

    creación componente virtual-assistant-tree

--- src/app/modules/virtual-assistant/components/virtual-assistant-tree/virtual-assistant-tree.module.ts
+++ src/app/modules/virtual-assistant/components/virtual-assistant-tree/virtual-assistant-tree.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// MATERIAL
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+// SHARED MODULE
+import { SharedModule } from 'src/app/shared/shared.module';
+
+// COMPONENT - VIRTUAL ASSISTANT TREE
+import { VirtualAssistantTreeComponent } from './component/virtual-assistant-tree.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VirtualAssistantTreeComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+
+    // MATERIAL
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  exports: [
+    VirtualAssistantTreeComponent
   ]
 })
 export class VirtualAssistantTreeModule { }
--- src/app/modules/virtual-assistant/virtual-assistant.module.ts
+++ src/app/modules/virtual-assistant/virtual-assistant.module.ts
@@ -5,6 +5,7 @@ import { VirtualAssistantPageComponent } from './virtual-assistant-page/virtual-
 //Angular Material
 import {MatExpansionModule} from '@angular/material/expansion';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { VirtualAssistantTreeModule } from './components/virtual-assistant-tree/virtual-assistant-tree.module';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     MatExpansionModule,
-    SharedModule
+    SharedModule,
+    VirtualAssistantTreeModule
   ]
 })
 export class VirtualAssistantModule { }
