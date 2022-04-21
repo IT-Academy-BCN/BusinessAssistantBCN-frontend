ee47c451c29a35d463609b7f5d2c7be6348d5ba1
Voluntata <voluntata@gmail.com>
2022-04-21 09:42:40 +0200

    Enviroment & mapbox refactoring

--- src/app/shared/mapbox/mapbox.component.ts
+++ src/app/shared/mapbox/mapbox.component.ts
@@ -1,7 +1,6 @@
 import { Component, AfterViewInit, ViewChild, ElementRef, Input } from "@angular/core";
 import Mapboxgl, { LngLatBounds, NavigationControl, GeolocateControl, Map, Popup, Marker } from "mapbox-gl";
-import { environment } from "src/environments/environment";
-import { LargeStablishmentModel } from '../../models/large-stablishment.model';
+import { BusinessWithAddressModel } from "src/app/models/common/businessWithAddressModel.model";
 
 @Component({
   selector: "app-mapbox",
@@ -11,9 +10,33 @@ import { LargeStablishmentModel } from '../../models/large-stablishment.model';
 export class MapboxComponent implements AfterViewInit {
   @ViewChild("mapDiv")
   mapDivElement!: ElementRef;
-  @Input() filteredResultsToPrintOnMap!: LargeStablishmentModel[];
+  @Input() filteredResultsToPrintOnMap!: BusinessWithAddressModel[];
   private map!: Map;
   private currentMarkers: Marker[] = [];
+
+  private MAPBOX_TOKEN: string = 'pk.eyJ1IjoianZyZnJlZWxhbmNlZGV2ZWxvcGVyIiwiYSI6ImNreTl4czUzMTAwNGQydnFsdmRhYXRvbDUifQ.TVL-2T184QdfXbze6VNw4A';
+  private MAPBOX_STYLE: string = 'mapbox://styles/mapbox/streets-v11';
+  private MAPBOX_ZOOM: number = 8;
+  private MAPBOX_ITAcademy_OBJECT: BusinessWithAddressModel = {
+    name: "IT Academy",
+    web: "bcn.cat/barcelonactiva",
+    email: "itacademy@barcelonactiva.cat",
+    phone: 932917610,
+    activities: [],
+    addresses: [
+      {
+        street_name: "Roc Boronat",
+        number: "117-127",
+        zip_code: "08018",
+        district_id: "04",
+        town: "BARCELONA",
+        location: {
+          x: 2.194060007737955,
+          y: 41.40389733660671,
+        },
+      },
+    ],
+  }
 
   constructor() { }
 
@@ -36,12 +59,12 @@ export class MapboxComponent implements AfterViewInit {
   }
 
   generateMap() {
-    Mapboxgl.accessToken = environment.MAPBOX_TOKEN;
+    Mapboxgl.accessToken = this.MAPBOX_TOKEN;
     this.map = new Map({
       container: this.mapDivElement.nativeElement,
-      style: "mapbox://styles/mapbox/streets-v11", // style URL }
-      center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], // starting center so it doesn't start from Germany
-      zoom: 8
+      style: this.MAPBOX_STYLE,  // style URL }
+      center: [this.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, this.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], // starting center so it doesn't start from Germany
+      zoom: this.MAPBOX_ZOOM
     });
 
     this.map.addControl(new NavigationControl());
@@ -58,7 +81,7 @@ export class MapboxComponent implements AfterViewInit {
   }
 
   // Function to create a single marker (with the marker's colour and the business (or user's coords) as parameters)
-  createANewMarker(markerColor: string, business?: LargeStablishmentModel, coord?: GeolocationCoordinates): void {
+  createANewMarker(markerColor: string, business?: BusinessWithAddressModel, coord?: GeolocationCoordinates): void {
 
     // Create a popup with the business's basic information
     const popup = new Popup().setHTML(
@@ -103,7 +126,7 @@ export class MapboxComponent implements AfterViewInit {
       () => {
         // this.map.flyTo(
         //   { center: [environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.x, environment.MAPBOX_ITAcademy_OBJECT.addresses[0].location.y], zoom: 11 })
-        this.createANewMarker("red", environment.MAPBOX_ITAcademy_OBJECT,);
+        this.createANewMarker("red", this.MAPBOX_ITAcademy_OBJECT);
       }
     );
   }
