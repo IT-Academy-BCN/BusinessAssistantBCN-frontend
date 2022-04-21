a002fe03a21ed126502961ff821d55961be75404
jriera <jordiriera81@gmail.com>
2022-04-21 09:02:06 +0200

    doc:Comment the environment  search template methods and variables

--- src/app/modules/home/my-environment-search/my-environment-search.component.ts
+++ src/app/modules/home/my-environment-search/my-environment-search.component.ts
@@ -9,30 +9,34 @@ import {EconomicActivityModel} from "../../../models/common/economic-activity.mo
   styleUrls: ['./my-environment-search.component.css']
 })
 export class MyEnvironmentSearchComponent implements OnInit {
-  zones:ZoneModel[];
-  activities:EconomicActivityModel[];
+  zones:ZoneModel[]; //zones will store all the available zones before any selection
+  activities:EconomicActivityModel[]; //activities will store all the available economic activities before any selection
 
   constructor(
       private commonService:CommonService
   ) { }
 
   ngOnInit(): void {
-    this.getAllActivities()
-    this.getAllZones()
+    this.getAllActivities() //gets all the activities available from the common service
+    this.getAllZones() //gets all the zones available from the common service
   }
 
   checkZones(zoneSelected: ZoneModel, event: any) {
     if (event.checked) {
+      //Adds the selected zone to the array zones in the common service to use it there as parameter
       this.commonService.zones.push(zoneSelected);
     } else {
+      //removes the zone if it is already in the common service array
       this.commonService.zones.splice(this.commonService.zones.indexOf(zoneSelected),1);
     }
   }
 
   checkActivities(activitySelected: EconomicActivityModel, event: any) {
     if (event.checked) {
+      //Adds the selected activity to the array zones in the common service to use it there as parameter
       this.commonService.activities.push(activitySelected);
     } else {
+      //removes the selected if it is already in the common service array
       this.commonService.activities.splice(this.commonService.activities.indexOf(activitySelected),1);
     }
   }
